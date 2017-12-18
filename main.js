a265bee7749bca6be50e9c981d4ad483aa5aeb33
Smriti <simmybajaj1603@gmail.com>
2017-12-19 04:33:40 +0530

    Ng-foodie initial

---
+++ main.js
@@ -0,0 +1,108 @@
+var foodieApp = angular.module('foodieApp',,['ngRoute']);
+foodieApp.config(function ($routeProvider) {
+	$routeProvider
+	.when('/',{
+		templateUrl: 'pages/login.html',
+		controller: 'loginController'
+	})
+	.when('/home',{
+		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	})
+})
+foodieApp.controller('loginController',function($scope) {
+    $scope.goToHome = function() {
+        //console.log('Do Something')
+        $location.url('home')
+	}
+})
+foodieApp.controller('mainController',function($scope) {
+	$scope.restaurants = [{
+        name: 'Farzi Cafe',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.2',
+        cuisines: 'Modern Indian',
+        cost: '2200',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'Garam  Dharam',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '3.9',
+        cuisines: 'Modern Indian',
+        cost: '1000',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'Social',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.9',
+        cuisines: 'Modern Indian',
+        cost: '1800',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'My Bar HQ',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.6',
+        cuisines: 'Modern Indian',
+        cost: '1500',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'F Bar',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.3',
+        cuisines: 'Modern Indian',
+        cost: '2100',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'MTV FLYP',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.2',
+        cuisines: 'Modern Indian',
+        cost: '2000',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'MRP',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.3',
+        cuisines: 'Modern Indian',
+        cost: '2100',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    },
+    {
+        name: 'Ware House Cafe',
+        address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+        location: 'Connaught Place',
+        category: 'Casual Dining, Bar',
+        vote: '4.5',
+        cuisines: 'Modern Indian',
+        cost: '2500',
+        hours: '12 Noon to 1 AM (Mon-Sun)',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+    }]
+})
